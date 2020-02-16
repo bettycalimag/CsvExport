aa8078e97f50f8da865cc4429a318a753d9305fe
bettycalimag <61044919+bettycalimag@users.noreply.github.com>
2020-02-17 01:28:49 +0800

    Add files via upload

---
+++ CsvController.cs
@@ -0,0 +1,107 @@
+﻿using Ionic.Zip;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OutputCsvFile.Datapersist;
+using OutputCsvFile.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OutputCsvFile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CsvController : ControllerBase
+    {
+        private readonly InfinityDataContext _context;
+        private IHostingEnvironment _env;
+
+        private enum identifier { TSQM2, DASS21, HIT6 }
+
+        public CsvController(InfinityDataContext context, IHostingEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        /// <summary>
+        ///  Performs an important function to zip and export the 3 csv files. 
+        ///  This calls the method ProcessWrite to save the files using stream then add the name of the 3 files to zip file.
+        ///  Then copy the files into memory stream and save to the zip.
+        ///  Return or output the zip file.
+        /// </summary>
+        [AllowAnonymous]
+        [HttpGet("", Name = "GetExport")]
+        public async Task<IActionResult> GetExport()
+        {
+            var contentRootPath = _env.ContentRootPath;
+
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AlternateEncodingUsage = ZipOption.AsNecessary;
+                zip.AddDirectoryByName("Files");
+
+
+                //Set the Name of Zip File.
+                string zipName = String.Format("Zip_{0}.zip", DateTime.Now.ToString("yyyy-MMM-dd-HHmmss"));
+                var memoryStream = new MemoryStream();
+                foreach (identifier val in Enum.GetValues(typeof(identifier)))
+                {
+                    var file = System.IO.Path.Combine(contentRootPath, val.ToString());
+                    ProcessWrite(file);
+                    zip.AddFile(val.ToString() + ".csv", "Files");
+
+                    using (var stream = new FileStream(file + ".csv", FileMode.Open))
+                    {
+                        await stream.CopyToAsync(memoryStream);
+                    }
+                }
+
+                //Save the Zip File to MemoryStream.
+                zip.Save(memoryStream);
+
+                memoryStream.Position = 0;
+                return File(memoryStream, System.Net.Mime.MediaTypeNames.Application.Octet, zipName);
+            }
+        }
+
+        /// <summary>
+        ///  Calls the database(query) and  returns a list.
+        ///  Calls the method FormatTsqm to format each data in the list to a string separated by semicolon
+        ///  Calls the method FormatHeader to format the header or column name in the csv
+        ///  Calls the method WriteTextAsync to transfer the data in the form of string into a file
+        ///  The string file is the name of the file.
+        /// </summary>
+        private async void ProcessWrite(string file)
+        {
+            var buffer = new StringBuilder();
+            List<CsvExportDto> list = await _context.Interviews
+                .Include(i => i.Patient)
+                .ThenInclude(n => n.Institute)
+                .Include(i => i.Questionnaires)
+                    .ThenInclude(q => q.Answers)
+                        .ThenInclude(a => a.Question)
+                .Include(i => i.Questionnaires)
+                    .ThenInclude(q => q.Definition)
+                .Where(i => i.IsCompleted)
+                .OrderBy(p => p.CompletionDate)
+                .Select(p => CsvExportDto.FromEntity(p, file))
+                .ToListAsync();
+
+            foreach (var tsqm in list)
+            {
+                CsvExportDto.FormatTsqm(buffer, tsqm);
+            }
+
+            buffer = CsvExportDto.FormatHeader(file).Append(buffer);
+
+            await CsvExportDto.WriteTextAsync(file, buffer.ToString());
+        }
+    }
+}
---
+++ CsvExportDto.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputCsvFile.Models
+{
+    public class CsvExportDto
+    {
+        public Guid? Id { get; set; }
+        public string PatientId { get; set; }
+        public string Institute { get; set; }
+        public string VisitId { get; set; }
+        public DateTime? CompleteDate { get; set; }
+        public IEnumerable<Answer> TsqmAnswers { get; set; }
+
+        /// <summary>
+        ///  Assign the CsvExportDto class properties to the values in database and returns a list.
+        ///  The entity is the model class
+        ///  The identifier is the identification or the names of the three files DASS, HIT, TSQM
+        /// </summary>
+        public static CsvExportDto FromEntity(Interview entity, string identifier)
+        {
+            if (entity == null)
+                return null;
+
+            return new CsvExportDto
+            {
+                Id = entity.Id,
+                PatientId = entity.Patient.Identifier,
+                Institute = entity.Patient.Institute.Identifier,
+                VisitId = entity.Identifier,
+                CompleteDate = entity.CompletionDate,
+                TsqmAnswers = entity.Questionnaires
+                .Where(q => q.Definition.Identifier == identifier && q.IsCompleted == true)
+                .Where(q => q.IsCompleted == true)
+                .SelectMany(q => q.Answers).ToList<Answer>()
+                .OrderBy(a => a.Question.Order)
+            };
+        }
+
+        /// <summary>
+        ///  Transfers the string to a file via FileStream, the filemode is in create mode meaning if the file already exists.
+        ///  it will be overwritten.
+        ///  The filePath is the path and name of the file to be created.
+        ///  The string text are the data that are in the form of string then converted into bytes then write into a file. 
+        /// </summary>
+        public static async Task WriteTextAsync(string filePath, string text)
+        {
+            byte[] encodedText = Encoding.Unicode.GetBytes(text);
+
+            using (FileStream sourceStream = new FileStream(filePath + ".csv",
+                FileMode.Create, FileAccess.Write, FileShare.None,
+                bufferSize: 4096, useAsync: true))
+            {
+                await sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
+                sourceStream.Flush();
+            };
+        }
+
+        /// <summary>
+        ///  A string builder that formats the contents\data into a string separated by semicolon.
+        ///  It separates each value in the list by a semicolon and output as a string.
+        ///  The stringbuilder buffer where data is to be stored
+        ///  The CsvExportDto tsqm is the IENUMERABLE class
+        /// </summary>
+        public static void FormatTsqm(StringBuilder buffer, CsvExportDto tsqm)
+        {
+            if (tsqm != null)
+            {
+                buffer.AppendFormat($"{tsqm.PatientId};");
+                buffer.AppendFormat($"{tsqm.Institute};");
+                buffer.AppendFormat($"{tsqm.VisitId};");
+                buffer.AppendFormat($"{(tsqm.CompleteDate?.ToString("dd.MM.yyyy hh:mm"))};");
+                foreach (var item in tsqm.TsqmAnswers)
+                {
+                    if (item.Value != null)
+                    {
+                        buffer.AppendFormat($"{item.Value.GetValue("v1").ToString().Trim(new Char[] { '[', ']', ' ' }).TrimStart().TrimEnd()};");
+                    }
+                    else
+                    {
+                        buffer.AppendFormat($"{9999};");
+                    }
+                }
+
+                buffer.Remove(buffer.Length - 1, 1);
+                buffer.Append("\n");
+            }
+        }
+
+        /// <summary>
+        ///  A string builder that formats the header column of the csv file separated by column.
+        ///  Returns a stringbuilder    
+        /// </summary>
+        public static StringBuilder FormatHeader(string file)
+        {
+            CsvExportDto tsqm = new CsvExportDto();
+            StringBuilder str = new StringBuilder();
+            int count = 0;
+
+            foreach (var prop in tsqm.GetType().GetProperties())
+            {
+
+                if (prop.Name == "TsqmAnswers")
+                {
+                    if (file == "TSQM2")
+                    {
+                        while (count < 11)
+                        {
+                            count++;
+                            str.AppendFormat($"{"Question" + count};");
+                        }
+                    }
+                    else if (file == "DASS21")
+                    {
+                        while (count < 21)
+                        {
+                            count++;
+                            str.AppendFormat($"{"Question" + count};");
+                        }
+                    }
+                    else
+                    {
+                        while (count < 6)
+                        {
+                            count++;
+                            str.AppendFormat($"{"Question" + count};");
+                        }
+                    }
+                }
+                else if (prop.Name != "Id")
+                { str.AppendFormat($"{prop.Name};"); }
+
+            }
+            str.Remove(str.Length - 1, 1);
+            str.Append("\n");
+            return str;
+        }
+    }
+}
